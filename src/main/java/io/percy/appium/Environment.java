7f7adf9a595eeab92f3f91e13d6a5c7bd463710c
ninadbstack <60422475+ninadbstack@users.noreply.github.com>
2022-11-29 17:38:23 +0530

    Release 0.0.4 (#27)
    * [maven-release-plugin] prepare release percy-appium-app-0.0.4
    
    * [maven-release-plugin] prepare for next development iteration
    
    * Updated verison across codebase

--- src/main/java/io/percy/appium/Environment.java
+++ src/main/java/io/percy/appium/Environment.java
@@ -4,7 +4,7 @@ import io.appium.java_client.AppiumDriver;
 
 public class Environment {
     private AppiumDriver driver;
-    public static final String SDK_VERSION = "0.0.3";
+    public static final String SDK_VERSION = "0.0.4";
     private static final String SDK_NAME = "percy-appium-app";
 
     public Environment(AppiumDriver driver) {
