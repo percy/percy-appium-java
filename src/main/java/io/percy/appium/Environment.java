c2f4d15a35b708aba308002fb86d37dd274dd3ff
Pankaj Yadav <pankajofficial443@gmail.com>
2022-12-20 18:10:47 +0530

    Version 0.0.5 bump (#33)
    * version bump
    
    * [maven-release-plugin] prepare release percy-appium-app-0.0.5
    
    * [maven-release-plugin] prepare for next development iteration

--- src/main/java/io/percy/appium/Environment.java
+++ src/main/java/io/percy/appium/Environment.java
@@ -4,7 +4,7 @@ import io.appium.java_client.AppiumDriver;
 
 public class Environment {
     private AppiumDriver driver;
-    public static final String SDK_VERSION = "0.0.4";
+    public static final String SDK_VERSION = "0.0.5";
     private static final String SDK_NAME = "percy-appium-app";
 
     public Environment(AppiumDriver driver) {
