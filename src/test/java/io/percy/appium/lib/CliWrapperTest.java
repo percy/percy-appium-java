da3d282e30358dfc03b7aa85125b21c6d2851049
pankaj443 <pankajofficial443@gmail.com>
2023-05-25 18:30:37 +0530

    remove imports by default

--- src/test/java/io/percy/appium/lib/CliWrapperTest.java
+++ src/test/java/io/percy/appium/lib/CliWrapperTest.java
@@ -1,15 +1,10 @@
 package io.percy.appium.lib;
 
-import org.aspectj.weaver.RuntimeVersion;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
-import com.github.tomakehurst.wiremock.client.WireMock;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.percy.appium.AppPercy;
@@ -25,20 +20,20 @@ public class CliWrapperTest {
         try {
             Integer version = Runtime.version().major();
             if (version > 10) {
-                WireMockServer server = new WireMockServer(5338);
+                com.github.tomakehurst.wiremock.WireMockServer server = new com.github.tomakehurst.wiremock.WireMockServer(5338);
                 JSONObject responseJsonObject = new JSONObject();
                 JSONObject buildObject = new JSONObject();
                 buildObject.put("id", "27591981");
                 buildObject.put("url", "https://percy.io/9560f98d/app-proj-temp/builds/27591981");
                 responseJsonObject.put("build", buildObject);
-                ResponseDefinitionBuilder mockResponse = new ResponseDefinitionBuilder();
+                com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder mockResponse = new com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder();
                 mockResponse.withHeader("x-percy-core-version", "1.2");
                 mockResponse.withStatus(200);
                 mockResponse.withBody(String.valueOf(responseJsonObject));
                 server.start();
-                WireMock.configureFor("127.0.0.1", 5338);
-                WireMock.stubFor(
-                        WireMock.get("/percy/healthcheck")
+                com.github.tomakehurst.wiremock.client.WireMock.configureFor("127.0.0.1", 5338);
+                com.github.tomakehurst.wiremock.client.WireMock.stubFor(
+                    com.github.tomakehurst.wiremock.client.WireMock.get("/percy/healthcheck")
                                 .willReturn(mockResponse)
                 );
         
