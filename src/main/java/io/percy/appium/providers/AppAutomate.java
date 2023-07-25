71bd287eceb8e430a5f9f359fbcae583f0eabd14
prklm10 <pradumraj360@gmail.com>
2023-07-25 15:58:11 +0530

    removing log

--- src/main/java/io/percy/appium/providers/AppAutomate.java
+++ src/main/java/io/percy/appium/providers/AppAutomate.java
@@ -97,7 +97,6 @@ public class AppAutomate extends GenericProvider {
             }
             String projectId = "percy-prod";
             if (Environment.getEnablePercyDev()) {
-                System.out.print("HELLO");
                 projectId = "percy-dev";
             }
             JSONObject arguments = new JSONObject();
