60c51549a196802100a1960af7c66c50f6a04473
Pankaj Yadav <pankajofficial443@gmail.com>
2023-09-08 14:56:20 +0530

    Add scrollviewOffset options (#125)
    * Add scrollviewoffset options
    
    * update readme
    
    * Remove debug logs

--- src/main/java/io/percy/appium/providers/AppAutomate.java
+++ src/main/java/io/percy/appium/providers/AppAutomate.java
@@ -105,6 +105,8 @@ public class AppAutomate extends GenericProvider {
             args.put("deviceHeight", deviceHeight);
             args.put("scollableXpath", options.getScrollableXpath());
             args.put("scrollableId", options.getScrollableId());
+            args.put("topScrollviewOffset", options.getTopScrollviewOffset());
+            args.put("bottomScrollviewOffset", options.getBottomScrollviewOffset());
             args.put("FORCE_FULL_PAGE", Environment.getForceFullPage());
             arguments.put("state", "screenshot");
             arguments.put("percyBuildId", Environment.getPercyBuildID());
