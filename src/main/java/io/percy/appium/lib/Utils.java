62109f8105f851ce328ec2981e80c921ab9a0814
prklm10 <pradumraj360@gmail.com>
2024-09-23 11:39:28 +0530

    adding support for android version 14

--- src/main/java/io/percy/appium/lib/Utils.java
+++ src/main/java/io/percy/appium/lib/Utils.java
@@ -12,6 +12,14 @@ public class Utils {
       if (matcher.find()) {
         return Integer.parseInt(matcher.group(1));
       }
+
+      // For android version 14 update
+      Pattern secondPattern = Pattern.compile("statusBars frame=\\[\\d+,\\d+\\]\\[\\d+,([\\d]+)\\]");
+      Matcher secondMatcher = secondPattern.matcher(input);
+      if (secondMatcher.find()) {
+        return Integer.parseInt(secondMatcher.group(1));
+      }
+
       return null; // Return null if no match found
     } catch (Exception ex) {
       AppPercy.log(ex.toString(), "debug");
@@ -27,6 +35,16 @@ public class Utils {
       if (matcher.find()) {
         int bottomCoordinate = Integer.parseInt(matcher.group(1));
         int topCoordinate = Integer.parseInt(matcher.group(2));
+        return topCoordinate - bottomCoordinate;
+      }
+
+      // For android version 14 update
+      Pattern secondPattern = Pattern.compile("navigationBars frame=\\[\\d+,([\\d]+)\\]\\[\\d+,([\\d]+)\\]");
+      Matcher secondMatcher = secondPattern.matcher(input);
+
+      if (secondMatcher.find()) {
+        int bottomCoordinate = Integer.parseInt(secondMatcher.group(1));
+        int topCoordinate = Integer.parseInt(secondMatcher.group(2));
         return topCoordinate - bottomCoordinate;
       }
       return null; // Return null if no match found
